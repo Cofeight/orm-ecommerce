3de04ec2af1b82924b5407d0e1498e9c4dd16911
Chris <chris.feight@gmail.com>
2021-11-10 22:32:32 -0800

    fixing issues

---
+++ README.md
@@ -0,0 +1,47 @@
+
+
+# [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT);
+
+# E-Commerce Backend (ORM)
+
+
+## Description
+    Creates a backend for an E-Commerce website to make my company more competitive within the market
+
+## Table of Contents
+
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Tests](#tests)
+* [Contributions](#contributions)
+* [Questions](#questions)
+
+
+## Installation
+    npm init, install mysql, express, sequelize, dotenv
+
+## Usage
+   ![gif displaying get paths](https://github.com/Cofeight/orm-ecommerce/blob/main/assets/get-paths.gif)
+   ![server/seeding gifs](https://github.com/Cofeight/orm-ecommerce/blob/main/assets/sqlseed-server.gif)
+
+## License
+    MIT
+
+## Tests
+    Currently, tests have not been developed for this project.
+    
+## Contributions
+    Users should feel free to reach out with any contributions.
+
+
+## Questions
+    With any questions, please contact the owner of this project by following the links below.
+
+## Contact Info
+
+## [ChrisOF@uw.edu](mailto:ChrisOF@uw.edu)
+
+## [Cofeight](https://github.com/Cofeight)
+    
---
+++ routes/api/category-routes.js
@@ -0,0 +1,78 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+
+router.get('/', (req, res) => {
+  // find all categories
+  // be sure to include its associated Products
+  Category.findAll({
+      include: [Product],
+    }).then(data => {res.status(200).json(data)})
+    .catch( (err) => {
+  res.status(500).json(err)
+  })
+});
+
+router.get('/:id', (req, res) => {
+  // find one category by its `id` value
+  // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [Product],
+  }).then(data => {res.status(200).json(data)})
+  .catch( (err) => {
+res.status(500).json(err)
+})
+});
+
+router.post('/', (req, res) => {
+  // create a new category
+  console.log(req.body);
+  Category.create(req.body)
+    .then((category) => {
+      res.status(200).json(category);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+});
+
+router.put('/:id', (req, res) => {
+  // update a category by its `id` value
+  console.log(req.body)
+  Category.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+  .then((category) => {
+    res.status(200).json(category);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(400).json(err);
+  });
+});
+
+router.delete('/:id', (req, res) => {
+  // delete a category by its `id` value
+  console.log(req.body)
+  Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+  .then((category) => {
+    res.status(200).json(category);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(400).json(err);
+  });
+});
+
+module.exports = router;
